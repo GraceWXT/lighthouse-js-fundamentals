249be1ba026d71889f2c960916819a393e7c8b67
Grace Wang <wangxiaotian2012@gmail.com>
2022-02-10 05:39:47 +0000

    Tinkering codes, up to Module 4 Functions Assignment

---
+++ tinkering.js
@@ -0,0 +1,49 @@
+/*
+function multiply(a, b) {
+  return a * b
+}
+const result = multiply(2, 4)
+console.log(result)
+
+sayHi("Julia");
+
+function sayHi(name) {
+  console.log(greeting + name);
+  var greeting;
+}
+*/
+
+/*
+let catSays = function (max) {
+  let catMessage = "";
+  for (let i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+};
+
+let catSings = function (max) {
+  let catLyric = "";
+  for (let i = 0; i < max; i++) {
+    catLyric += "la";
+  }
+  return catLyric;
+};
+
+function helloCat(callbackFunc, max) {
+  return "Hello " + callbackFunc(max);
+}
+
+console.log(helloCat(catSings, 3))
+*/
+
+function helloCat(catFunc, max) {
+  return "Hello" + catFunc(3);
+}
+console.log(helloCat(function catSing(max) {
+  let catLyric = "";
+  for (let i = 0; i < max; i++) {
+    catLyric += "la";
+  }
+  return catLyric;
+}, 3))
