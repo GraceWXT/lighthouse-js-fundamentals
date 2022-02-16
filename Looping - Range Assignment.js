a611d212dd5ece4705f78c8db5ed61530738e5ce
Grace Wang <wangxiaotian2012@gmail.com>
2022-02-16 06:18:05 +0000

    Range assignment

---
+++ Looping - Range Assignment.js
@@ -0,0 +1,16 @@
+const range = function (start, end, step) {
+  let array = [];
+  if (step <= 0 || start > end || start === undefined || end === undefined || step === undefined)
+    return array;
+  else {
+    while (start <= end) {
+      array.push(start);
+      start += step;
+    }
+    return array;
+  }
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
